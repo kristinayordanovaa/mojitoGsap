977017da262ad6af3b3ee435bd37c6086ef6b839
Kristina Yordanova <kristinayordanovaa@gmail.com>
2026-03-16 19:08:08 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { SplitText } from 'gsap/SplitText';
+import Navbar from './components/Navbar';
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+<main>
+ <Navbar/>
+
+</main>
+);
+}
+
+export default App;
