255950ed6e832748b59074b9a438c193d378cb74
Kristina Yordanova <kristinayordanovaa@gmail.com>
2026-03-17 13:54:34 +0200

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import { SplitText } from 'gsap/SplitText';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import { Cocktails } from './components/Cocktails';
+import { About } from './components/About';
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
 <main>
- <Navbar/>
- <Hero/>
-<Cocktails/>
+    <Navbar />
+    <Hero />
+    <Cocktails />
+    <About />
 </main>
 );
 }
 
-export default App;
+export default App; 
